b792bf809cc9eb9aa554ea92628a27f3685d1abd
KgMoney38 <kodydavison@hotmail.com>
2025-01-18 21:43:03 -0600

    Initial Classes

---
+++ Purse.java
@@ -0,0 +1,25 @@
+public class Purse {
+
+    double amt;
+
+  public void add(Denomination denomination, int num )
+    {
+
+
+    }
+
+    public void remove(Denomination denomination, int num )
+    {
+
+    }
+
+    public double getValue()
+    {
+      return amt;
+    }
+
+    public String toString()
+    {
+        return amt + "";
+    }
+}
---
+++ Register.java
@@ -0,0 +1,17 @@
+public class Register
+{
+
+   public static void main(String[] args)
+    {
+        Register register = new Register();
+
+    }
+
+    public Purse makeChange(double amt)
+    {
+
+        Purse purse = new Purse();
+
+        return purse;
+    }
+}
